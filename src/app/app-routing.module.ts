4d388e7baa4973cbf9708c2b8dd7151504c2e4b7
LCS32 <lucaspontesmarques23@gmail.com>
2022-11-10 20:54:56 +0100

    3 vistas listas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { GarantiasComponent } from './garantias/garantias.component';
+import { NotesComponent } from './notes/notes.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
+import { TodosComponent } from './todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'bookmarks', component: BookmarksComponent },
+  { path: 'todos', component: TodosComponent },
+  { path: 'notes', component: NotesComponent },
+  { path: 'pedidos', component: PedidosComponent },
+  { path: 'clientes', component: ClientesComponent },
+  { path: 'garantias', component: GarantiasComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { TodosComponent } from './todos/todos.component';
+import { NotesComponent } from './notes/notes.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { GarantiasComponent } from './garantias/garantias.component';
+import { BookmarkTileComponent } from './bookmark-tile/bookmark-tile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    BookmarksComponent,
+    TodosComponent,
+    NotesComponent,
+    PedidosComponent,
+    ClientesComponent,
+    GarantiasComponent,
+    BookmarkTileComponent
   ],
   imports: [
     BrowserModule,
